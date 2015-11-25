91310c5a69ccc25a0a22998d43c18cebfac564d2
oilandsugar <tommy_pm@hotmail.com>
2015-11-25 09:26:20 -0500

    add list of exercices, map data to views

---
+++ js/animations.js
@@ -0,0 +1,7 @@
+jQuery(function($) {
+  $(document).ready(function(){
+    $('#toggle-exercice-list').click(function(){
+      $('.all-exercices').slideToggle();
+    });
+  });
+});
--- js/workout-app/app.js
+++ js/workout-app/app.js
@@ -1,1 +1,5 @@
 angular.module('workout-app', []);
+
+$('toggle-exercice-list').click(function(){
+  $('.all-exercices').slideToggle();
+});
