a83e0f8b5b8681312f20e0bf7fb9425453bbb8a7
Jonathan Girard Viau <girardvjonathan@gmail.com>
2015-12-21 16:34:33 -0500

    Added login with the Django API

--- js/workout-app/app.js
+++ js/workout-app/app.js
@@ -1,71 +1,91 @@
 angular.module('workout-app', ['ui.router', 'timer'])
-
-angular.module('workout-app').config(function($stateProvider, $urlRouterProvider) {
-  //
-  // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/");
-  //
-  // Now set up the states
-  $stateProvider
-    .state('home', {
-      url: "/",
-      templateUrl: "js/workout-app/partials/home.html"
+    .run(function ($http, $rootScope) {
+        if ($rootScope.username) {
+            console.log('adding authorization cause username is set');
+            $http.defaults.headers.common.Authorization = $rootScope.username + ':' + $rootScope.password;
+            return;
+        }
     })
-    .state('workout', {
-      url: "/workout",
-      templateUrl: "js/workout-app/partials/workout.html"
-    })
-    .state('workout.list', {
-      url: "list",
-      templateUrl: "js/workout-app/partials/home.list.html",
-      controller: function($scope) {
-        $scope.exercices = [
-          {
-            name: 'squat',
-            reps: 10,
-    	      duration: 10,
-    	      intensity: 7,
-    	      type: 'type',
-            body_parts: ['legs'],
-    	      instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida libero turpis, nec mattis odio imperdiet at. Maecenas venenatis tincidunt lacinia. Phasellus finibus augue metus, vel lobortis dui rutrum vitae.',
-    	      schema: 'test',
-            calories_per_time: 30
-          },
-          {
-            name: 'push up',
-            reps: 15,
-    	      duration: 5,
-    	      intensity: 8,
-    	      type: 'type',
-            body_parts: ['arms'],
-    	      instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida libero turpis, nec mattis odio imperdiet at. Maecenas venenatis tincidunt lacinia. Phasellus finibus augue metus, vel lobortis dui rutrum vitae.',
-    	      schema: 'test',
-            calories_per_time: 40
-          },
-          {
-            name: 'something',
-            reps: 200,
-    	      duration: 20,
-    	      intensity: 8,
-    	      type: 'type',
-            body_parts: ['arms'],
-    	      instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida libero turpis, nec mattis odio imperdiet at. Maecenas venenatis tincidunt lacinia. Phasellus finibus augue metus, vel lobortis dui rutrum vitae.',
-    	      schema: 'test',
-            calories_per_time: 200
-          }
-        ];
-      }
-    })
-    .state('profile', {
-      url: "/profile",
-      templateUrl: "js/workout-app/partials/profile.html"
-    })
-    .state('login', {
-      url: "/login",
-      templateUrl: "js/workout-app/partials/login.html"
-    })
-    .state('register', {
-      url: "/register",
-      templateUrl: "js/workout-app/partials/register.html"
+    .service('User', function () {
+        this.getUser = function () {
+            return this.user;
+        };
+        this.setUser = function (user) {
+            this.user = user;
+        };
     });
+angular.module('workout-app').config(function ($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/home");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('base', {
+            url: "/",
+            templateUrl: "js/workout-app/partials/base.html",
+            controller: "MainController as main"
+        })
+        .state('base.home', {
+            url: "/home",
+            templateUrl: "js/workout-app/partials/home.html"
+        })
+        .state('workout', {
+            url: "/workout",
+            templateUrl: "js/workout-app/partials/workout.html"
+        })
+        .state('workout.list', {
+            url: "list",
+            templateUrl: "js/workout-app/partials/home.list.html",
+            controller: function ($scope) {
+                $scope.exercices = [
+                    {
+                        name: 'squat',
+                        reps: 10,
+                        duration: 10,
+                        intensity: 7,
+                        type: 'type',
+                        body_parts: ['legs'],
+                        instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida libero turpis, nec mattis odio imperdiet at. Maecenas venenatis tincidunt lacinia. Phasellus finibus augue metus, vel lobortis dui rutrum vitae.',
+                        schema: 'test',
+                        calories_per_time: 30
+                    },
+                    {
+                        name: 'push up',
+                        reps: 15,
+                        duration: 5,
+                        intensity: 8,
+                        type: 'type',
+                        body_parts: ['arms'],
+                        instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida libero turpis, nec mattis odio imperdiet at. Maecenas venenatis tincidunt lacinia. Phasellus finibus augue metus, vel lobortis dui rutrum vitae.',
+                        schema: 'test',
+                        calories_per_time: 40
+                    },
+                    {
+                        name: 'something',
+                        reps: 200,
+                        duration: 20,
+                        intensity: 8,
+                        type: 'type',
+                        body_parts: ['arms'],
+                        instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida libero turpis, nec mattis odio imperdiet at. Maecenas venenatis tincidunt lacinia. Phasellus finibus augue metus, vel lobortis dui rutrum vitae.',
+                        schema: 'test',
+                        calories_per_time: 200
+                    }
+                ];
+            }
+        })
+        .state('profile', {
+            url: "/profile",
+            templateUrl: "js/workout-app/partials/profile.html"
+        })
+        .state('base.login', {
+            url: "/login",
+            templateUrl: "js/workout-app/partials/login.html",
+            controller: "loginCtrl as login"
+        })
+        .state('register', {
+            url: "/register",
+            templateUrl: "js/workout-app/partials/register.html"
+        });
 });
---
+++ js/workout-app/controllers/loginCtrl.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('workout-app')
+        .controller('loginCtrl', ['$http', '$stateParams', '$rootScope', '$state', 'User', Login]);
+
+    function Login($http, $stateParams, $rootScope, $state, User) {
+        console.log('in controller login');
+        var vm = this;
+        vm.login = function (user) {
+            $http({
+                method: 'POST',
+                url: 'http://localhost:8888/api/auth/',
+                headers: {
+                    'Authorization': "Basic " +
+                    btoa(user.username + ':' + user.password)
+                }
+                //data: {date: '2013-01-01T20:00', duration:'20:00'}
+            }).then(function successCallback(response) {
+                User.setUser(response.data);
+                $state.go('base.home', {}, {reload: true});
+
+            }, function errorCallback(response) {
+                alert('Wrong credential');
+            });
+
+        }
+    }
+})();
